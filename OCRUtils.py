9c2ac0be46784e1d1a38173bf4fc549c67a14395
twikura <rainbowtrash2333@gmail.com>
2025-05-16 15:58:51 +0800

    update .gitignore

--- OCRUtils.py
+++
@@ -1,9 +0,0 @@
-import easyocr
-
-if __name__ == '__main__':
-    # 创建reader对象
-    reader = easyocr.Reader(['ch_sim'])
-    # 读取图像
-    result = reader.readtext(r'D:\temp\111.jpg')
-    # 结果
-    print(result)
